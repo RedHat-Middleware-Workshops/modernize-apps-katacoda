e4cd1436925f88e9c6a6d32ebf15adbefa82288a
Thomas Qvarnström <tqvarnst@redhat.com>
2018-02-14 12:31:35 +0100

    asdf

--- scenarios/internal-scenario-01/set-env.sh
+++ scenarios/internal-scenario-01/set-env.sh
@@ -2,5 +2,11 @@ export JAVA_HOME=$(jrunscript -e 'java.lang.System.out.println(java.lang.System.
 cd ${HOME}/projects/monolith
 git pull --quiet
 git checkout solution
+
+if [ ! -f ~/.init.done ]; then
+  printf "Waiting for the Environment to configure "
+  while [ ! -f ~/.init.done ]; do printf "."; sleep 3; done
+fi
+
 clear; echo; echo -e "\033[0;32mYour ip-address for this cluster is $(hostname -i), please record this so that the instructor can reboot your machine if needed.\033[0m";echo
 
